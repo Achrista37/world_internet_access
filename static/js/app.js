f8addd2763f755950d362a83d4b160d2b69476c2
andriani <a.christanty@gmail.com>
2022-04-23 16:05:03 +0800

    updated js file on map vis

--- static/js/app.js
+++ static/js/app.js
@@ -1,26 +1,43 @@
-d3.csv('https://raw.githubusercontent.com/plotly/datasets/master/2010_alcohol_consumption_by_country.csv', function(err, rows){
-      function unpack(rows, key) {
-          return rows.map(function(row) { return row[key]; });
-      }
+    d3.json("internet_with_countrycodes.json", function(err, rows) {
+        console.log(rows);
+        function unpack(rows, key) {
+            return rows.map(function(row) { return row[key]; });
+        }
+  
+      var data = [{
+          type: 'choropleth',
+          locationmode: 'country codes',
+          locations: unpack(rows, 'Country_Code'),
+          z: unpack(rows, '2019_Internet_Use_Perc'),
+          text: unpack(rows, 'Country_Name'),
+          autocolorscale: false,
+            reversescale: true,
+            marker: {
+                line: {
+                    color: 'rgb(0,191,255)',
+                    width: 0.5
+                }
+            },
+            tick0: 0,
+            zmin: 0,
+            dtick: 1000,
+            colorbar: {
+                autotic: false,
+                tickprefix: '%',
+                title: '% Population of the Country Using Internet'
+            }
+      }];
+  
+      var layout = {
+        title: '2019 World Internet Use',
+        geo: {
+            projection: {
+                type: 'orthographic'
+            }
+        }
+      };
+  
+      Plotly.newPlot("myDiv", data, layout, {showLink: false});
+    });
 
-    var data = [{
-        type: 'choropleth',
-        locationmode: 'country names',
-        locations: unpack(rows, 'location'),
-        z: unpack(rows, 'alcohol'),
-        text: unpack(rows, 'location'),
-        autocolorscale: true
-    }];
 
-    var layout = {
-      title: 'Pure alcohol consumption<br>among adults (age 15+) in 2010',
-      geo: {
-          projection: {
-              type: 'robinson'
-          }
-      }
-    };
-
-    Plotly.newPlot("myDiv", data, layout, {showLink: false});
-
-});
