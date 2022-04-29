d99d913974e7483b044f7f2d0d3fe525893ca5ef
andriani <a.christanty@gmail.com>
2022-04-29 13:53:46 +0800

    broken html

--- Dashboard-anime-plotlymap/app.js
+++ Dashboard-anime-plotlymap/app.js
@@ -1,3 +1,90 @@
+var data = null;
+
+function chartEarth(yearof) {
+d3.json("https://world-internet-access.herokuapp.com/api/dashboard", function(err, rows) {
+        data = rows;
+        console.log(data)
+        console.log(rows);
+        function unpack(rows, key) {
+            return rows.map(function(row) { return row[key]; });
+        }
+        
+        var yearstr = `Internet_Use_Perc_${yearof}` ;
+        console.log(yearstr)
+
+      var data = [{
+          type: 'choropleth',
+          locationmode: 'country codes',
+          locations: unpack(rows, 'Abbr'),
+          z: unpack(rows, yearstr),
+          text: unpack(rows, 'Country'),
+          autocolorscale: false,
+            reversescale: true,
+            marker: {
+                line: {
+                    color: 'rgb(0,191,255)',
+                    width: 0.5
+                }
+            },
+            tick0: 0,
+            zmin: 0,
+            dtick: 1000,
+            colorbar: {
+                autotic: false,
+                tickprefix: '%',
+                title: '% Population of the Country Using Internet'
+            }
+      }];
+      console.log(rows)
+      var layout = {
+        title: '2019 World Internet Use',
+        geo: {
+            projection: {
+                type: 'orthographic'
+            }
+        }
+      };
+  
+      Plotly.newPlot("myDiv", data, layout, {showLink: false});
+    });
+};
+
+
+
+
+//handle selected option
+function optionChanged(newVariable) {
+    console.log(newVariable);
+    chartEarth(newVariable);  
+  }
+ 
+  function initannumdropdown() {
+    // Use D3 to select the dropdown menu
+    var dropdownMenu = d3.select("#select-year");
+    var annum = ["2011","2012","2013","2014","2015","2016","2017","2018","2019"]; 
+    // dropdownMenu.html("");
+      //populate drop down menu
+      annum.forEach((name) => {
+        dropdownMenu
+          .append('option')
+        .text(name) // text showed in the menu
+        .property("value", name);
+        // console.log(name);
+      });
+      //get the graph to display the first participant's data when the page initially loads
+    //   var uponLoadingpage = annum[0];
+    //   console.log(uponLoadingpage);
+    //   createBarcharts(uponLoadingpage);
+    chartEarth(annum[0]);  
+  }
+
+
+
+
+initannumdropdown();
+
+
+
 /*
 d3.json("https://world-internet-access.herokuapp.com/api/dashboard", function(err, rows) {
         console.log(rows);
@@ -57,30 +144,8 @@ function initannumdropdown() {
     };
     */
 //////////////////////////////////////////////////////////////////////////
-    function initannumdropdown() {
-        // Use D3 to select the dropdown menu
-        var dropdownMenu = d3.select("#select-year");
-        var annum = ["2011","2012","2013","2014","2015","2016","2017","2018","2019"]; 
-        
-          //populate drop down menu
-          annum.forEach((name) => {
-            dropdownMenu
-              .append('option')
-            .text(name) // text showed in the menu
-            .property("value", name);
-            // console.log(name);
-          });
-          //get the graph to display the first participant's data when the page initially loads
-        //   var uponLoadingpage = annum[0];
-        //   console.log(uponLoadingpage);
-        //   createBarcharts(uponLoadingpage);
-        chartEarth(annum[0]);  
-      }
+   
 
-
-
-
-initannumdropdown();
 
 /*
 d3.json("https://world-internet-access.herokuapp.com/api/dashboard").then((data) => {
@@ -136,57 +201,3 @@ d3.json("https://world-internet-access.herokuapp.com/api/dashboard").then((mydat
 ;
 }
 */
-
-function chartEarth(yearof) {
-d3.json("https://world-internet-access.herokuapp.com/api/dashboard", function(err, rows) {
-        console.log(rows);
-        function unpack(rows, key) {
-            return rows.map(function(row) { return row[key]; });
-        }
-        
-        var yearstr = 'Internet_Use_Perc_' + yearof ;
-        console.log(yearstr)
-
-      var data = [{
-          type: 'choropleth',
-          locationmode: 'country codes',
-          locations: unpack(rows, 'Abbr'),
-          z: unpack(rows, yearstr),
-          text: unpack(rows, 'Country'),
-          autocolorscale: false,
-            reversescale: true,
-            marker: {
-                line: {
-                    color: 'rgb(0,191,255)',
-                    width: 0.5
-                }
-            },
-            tick0: 0,
-            zmin: 0,
-            dtick: 1000,
-            colorbar: {
-                autotic: false,
-                tickprefix: '%',
-                title: '% Population of the Country Using Internet'
-            }
-      }];
-  
-      var layout = {
-        title: '2019 World Internet Use',
-        geo: {
-            projection: {
-                type: 'orthographic'
-            }
-        }
-      };
-  
-      Plotly.newPlot("myDiv", data, layout, {showLink: false});
-    });
-};
-
-//handle selected option
-function optionChanged(newVariable) {
-    console.log(newVariable);
-    chartEarth(newVariable);  
-  }
- 
