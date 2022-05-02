9250385f252524de85e90c227092541ba5bdf003
VIRGINIA WAINAINA MWAPE <41284404+VirginiaMwape@users.noreply.github.com>
2022-05-02 21:25:03 +0800

    Add files via upload

--- ScatterDraft-Virg-2/ScatterPlot_Draft/src/assets/js/app.js
+++ ScatterDraft-Virg-2/ScatterPlot_Draft/src/assets/js/app.js
@@ -1,226 +1,155 @@
 
- 
-// @TODO: YOUR CODE HERE!
-var svgWidth = 1500;
-var svgHeight = 900;
+function chartEarth2(InterYear) {
+  d3.json("https://world-internet-access.herokuapp.com/api/dashboard").then(rows => {
+      // globaldata = rows;
+      function unpack(rows, key) {
+          return rows.map(function (row) { return row[key]; });
+      };
 
-var margin = {
-  top: 20,
-  right: 40,
-  bottom: 80,
-  left: 100
+      var Interstr = `GDP_${InterYear}`;
+      console.log(yearstr)
+
+////////////ANDRIANI PLOT 
+
+
+      var data = [{
+          type: 'choropleth',
+          locationmode: 'country codes',
+          locations: unpack(rows, 'Abbr'),
+          z: unpack(rows, Interstr),
+          text: unpack(rows, 'Country'),
+          autocolorscale: false,
+          reversescale: true,
+          marker: {
+              line: {
+                  color: 'rgb(0,191,255)',
+                  width: 0.5
+              }
+          },
+          tick0: 0,
+          zmin: 0,
+          dtick: 1000,
+          colorbar: {
+              autotic: false,
+              tickprefix: '%',
+              title: 'GDP of the Country Using Internet'
+         
+          }
+      }];
+      console.log(rows)
+      var layout = {
+          title: `${InterYear} GDP`,
+          font: {
+              family: 'Courier New, monospace',
+              size: 12
+          
+          },
+          autosize: false,
+          width: 700,
+          paper_bgcolor:'rgb(255, 229, 249)',
+          plot_bgcolor:'rgb(245, 245, 252)',
+          height: 550,
+          margin: {
+              l: 20,
+              r: 30,
+              b: 5,
+              t: 30,
+              pad: 2
+            },
+          geo: {
+              projection: {
+                  type: 'hammer'
+              }
+          }
+      };
+
+      Plotly.newPlot("myDiv", data, layout, { showLink: false });
+
+////////////////// ASH PLOT
+
+      var income = unpack(rows, 'IncomeGroup')
+      // console.log(income)
+      newincome = []
+      for (i = 0; i < income.length; i++) {
+
+          if (income[i] == "High income") {
+              newincome.push('rgb(175, 175, 218,1');
+          }
+          else if (income[i] == 'Upper middle income') {
+              newincome.push('rgb(141, 205, 209,1)');
+          }
+          else if (income[i] == 'Lower middle income') {
+              newincome.push('rgb(214, 161, 202,1');
+          }
+          else if (income[i] == 'Low income') {
+              newincome.push('rgb(233, 175, 142,1');
+          }
+      }
+      var trace1 = {
+          x: unpack(rows, 'Country'),
+          y: unpack(rows, yearstr),
+          type: 'bar',
+          marker: {
+              color: newincome,
+          }
+        };
+        
+      var data2 = [trace1];
+
+      var layout2 = {
+
+          title: `${yearof} World Internet Use by Income Group`,
+          font: {
+              family: 'Courier New, monospace',
+              size: 12
+          
+          },
+          width:1370,
+          autosize: true,
+          showlegend:'true',
+          height: 400,
+          barmode: 'relative',
+          paper_bgcolor:'rgb(255, 227, 211)',
+          plot_bgcolor:'rgb(255, 227, 211)',
+          xaxis: {
+              title: 'Country',
+              font: {
+                family: 'Courier New, monospace',
+                size: 12,
+                color: 'black'
+              },
+              tickmode: 'array',
+              
+              ticks: 'outside',
+              tickangle: 90,
+              tickcolor: '#000',
+              textfont_size:7, 
+              tickvals:unpack(rows, 'Country'),
+              ticktext:unpack(rows, 'Country'),
+            },
+            yaxis:{
+                range:[0,100],
+                title: '(%) Population using the Internet',
+                font: {
+                    family: 'Courier New, monospace',
+                    size: 12,
+                    color: 'black'
+              },
+
+            }
+
+          };
+        
+      Plotly.newPlot('barchart', data2, layout2);
+        
+  });
 };
 
-var width = svgWidth - margin.left - margin.right;
-var height = svgHeight - margin.top - margin.bottom;
-
-// Create an SVG wrapper, append an SVG group that will hold our chart,
-// and shift the latter by left and top margins.
-var svg = d3
-  .select("#scatter")
-  .append("svg")
-  .attr("width", svgWidth)
-  .attr("height", svgHeight);
-
-// Append an SVG group
-var chartGroup = svg.append("g")
-  .attr("transform", `translate(${margin.left}, ${margin.top})`);
-
-// Initial Params
-var chosenXAxis = "population_2011";
-var chosenxAxis = "GDP_2011";
-
-// function used for updating x-scale var upon click on axis label
-function xScale(peopleData, chosenXAxis) {
-  // create scales
-  var xLinearScale = d3.scaleLinear()
-    .domain([d3.min(peopleData, d => d[chosenXAxis]),
-      d3.max(peopleData, d => d[chosenXAxis])
-    ])
-    .range([0, width]);
-
-  return xLinearScale;
-
-}
-
-// function used for updating xAxis var upon click on axis label
-function renderAxes(newXScale, xAxis) {
-  var bottomAxis = d3.axisBottom(newXScale);
-
-  xAxis.transition()
-    .duration(1000)
-    .call(bottomAxis);
-
-  return xAxis;
-}
-
-// function used for updating circles group with a transition to
-// new circles
-function renderCircles(circlesGroup, newXScale, chosenXAxis) {
-
-  circlesGroup.transition()
-    .duration(1000)
-    .attr("cx", d => newXScale(d[chosenXAxis]));
-
-  return circlesGroup;
-}
-
-// function used for updating circles group with new tooltip
-function updateToolTip(chosenXAxis, circlesGroup) {
-
-  if (chosenXAxis === "GDP") {
-    var label = "GDP:";
-  }
-  else {
-    var label = "Population: ";
-  }
-
-  var toolTip = d3.tip()
-    .attr("class", "d3-tip")
-    .offset([100, -10])
-    //.offset([0,5])
-    .html(function(d) {
-      return (`${d.Country}<br>Internet_Usage_Perc_: ${parseFloat(d.Internet_Use_Perc_).toFixed(1)}%<br>${label} ${d[chosenXAxis]}`);
-    });
-
-
-  circlesGroup.call(toolTip);
-
-  circlesGroup.on("mouseover", function(data) {
-    toolTip.show(data);
-  })
-    // onmouseout event
-    .on("mouseout", function(dataTip, index) {
-      toolTip.hide(dataTip);
-    });
-
-  return circlesGroup;
-}
-
-// Read CSV
-  d3.json("https://world-internet-access.herokuapp.com/api/dashboard").then(function(data) {
-    //Test data connection
-    console.log(data); //variables
-
-    data.forEach(function(data) {
-        data.Internet = +data.Internet_Use_Perc_2011/100;
-        data.GDP = +data.GDP_2011;
-        data.population = +data.population_2011;
-        data.abbr = data.Abbr;
-    })
-    // xLinearScale function above csv import
-    var xLinearScale = xScale(data, chosenXAxis);
-  
-    // Create y scale function
-    var yLinearScale = d3.scaleLinear()
-        .domain([0, d3.max(data, d => d.Internet)])
-        .range([height, 0]);
-  
-    // Create initial axis functions
-    var bottomAxis = d3.axisBottom(xLinearScale);
-    var leftAxis = d3.axisLeft(yLinearScale);
-  
-    // append x axis
-    var xAxis = chartGroup.append("g")
-        .classed("x-axis", true)
-        .attr("transform", `translate(0, ${height})`)
-        .call(bottomAxis);
-  
-    // append y axis
-    chartGroup.append("g")
-        .call(leftAxis
-          .ticks(15)
-          .tickFormat(d3.format(",.2%")));
-  
-    // append initial circles
-    var circlesGroup = chartGroup.selectAll("circle")
-        .data(data)
-        .enter()
-        .append("circle")
-        .attr("cx", d => xLinearScale(d[chosenXAxis]))
-        .attr("cy", d => yLinearScale(d.Internet))
-        .attr("r", 12)
-        .attr("fill", "blue")
-        .attr("opacity", ".5");
-    //append abbreviations    
-  
-    // Create group for  2 x- axis labels
-    var labelsGroup = chartGroup.append("g")
-        .attr("transform", `translate(${width / 2}, ${height + 20})`);
-  
-    var incomeLengthLabel = labelsGroup.append("text")
-        .attr("x", 0)
-        .attr("y", 20)
-        .attr("value", "population") // value to grab for event listener
-        .classed("active", true)
-        .text("Population");
-  
-    var GDPLabel = labelsGroup.append("text")
-        .attr("x", 0)
-        .attr("y", 40)
-        .attr("value", "GDP") // value to grab for event listener
-        .classed("inactive", true)
-        .text("Gross Domestic Product");
-  
-    // append y axis
-    chartGroup.append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 0 - margin.left)
-        .attr("x", 0 - (height / 2))
-        .attr("dy", "1em")
-        .classed("active", true)
-        .classed("axis-text", true)
-        .text("Internet Usage");
-    // updateToolTip function above csv import
-    var circlesGroup = updateToolTip(chosenXAxis, circlesGroup);
-
-  
-    // x axis labels event listener
-    labelsGroup.selectAll("text")
-        .on("click", function() {
-        // get value of selection
-        var value = d3.select(this).attr("value");
-        if (value !== chosenXAxis) {
-  
-            // replaces chosenXAxis with value
-            chosenXAxis = value;
-  
-            // updates x scale for new data
-            xLinearScale = xScale(data, chosenXAxis);
-  
-            // updates x axis with transition
-            xAxis = renderAxes(xLinearScale, xAxis);
-  
-            // updates circles with new x values
-            circlesGroup = renderCircles(circlesGroup, xLinearScale, chosenXAxis);
-  
-            // updates tooltips with new info
-            circlesGroup = updateToolTip(chosenXAxis, circlesGroup);
-  
-            // changes classes to change bold text
-            if (chosenXAxis === "GDP") {
-            GDPLabel
-                .classed("active", true)
-                .classed("inactive", false);
-            incomeLengthLabel
-                .classed("active", false)
-                .classed("inactive", true);
-            }
-            else {
-            GDPLabel
-                .classed("active", false)
-                .classed("inactive", true);
-            incomeLengthLabel
-                .classed("active", true)
-                .classed("inactive", false);
-            }
-        }
-        });
-    });
 
  //handle selected option
  function optionChanged(newVariable) {
   console.log(newVariable);
+  chartEarth2(newVariable);
 }
 
 function initannumdropdown() {
